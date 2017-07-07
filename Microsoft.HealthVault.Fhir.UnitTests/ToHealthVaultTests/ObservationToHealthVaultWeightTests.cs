1cabc9f91440552209da3c85b95b5c2836b809d8
Brandon Pollett <brandon.pollett@gmail.com>
2017-07-07 12:57:41 -0700

    Implement body height observation

--- Microsoft.HealthVault.Fhir.UnitTests/ToHealthVaultTests/ObservationToHealthVaultWeightTests.cs
+++ Microsoft.HealthVault.Fhir.UnitTests/ToHealthVaultTests/ObservationToHealthVaultWeightTests.cs
@@ -35,6 +35,20 @@ namespace Microsoft.HealthVault.Fhir.UnitTests.ToHealthVaultTests
             Assert.AreEqual("kg", weight.Value.DisplayValue.UnitsCode);
         }
 
+        [TestMethod]
+        public void HeightToHealthVault_Successful()
+        {
+            var json = SampleUtil.GetSampleContent("FhirHeight.json");
+
+            var fhirParser = new FhirJsonParser();
+            var observation = fhirParser.Parse<Observation>(json);
+
+            var height = observation.ToHealthVault() as Height;
+            Assert.IsNotNull(height);
+            Assert.AreEqual(1.73, height.Value.Meters);
+            Assert.AreEqual("m", height.Value.DisplayValue.Units);
+            Assert.AreEqual("m", height.Value.DisplayValue.UnitsCode);
+        }
 
         [TestMethod]
         public void BloodGlucoseToHealthVault_Successful()
@@ -59,7 +73,8 @@ namespace Microsoft.HealthVault.Fhir.UnitTests.ToHealthVaultTests
             var samples = new List<string>()
             {
                 SampleUtil.GetSampleContent("FhirBloodGlucose.json"),
-                SampleUtil.GetSampleContent("FhirWeight.json")
+                SampleUtil.GetSampleContent("FhirWeight.json"),
+                SampleUtil.GetSampleContent("FhirHeight.json"),
             };
 
             var list = new List<ThingBase>();
@@ -68,11 +83,16 @@ namespace Microsoft.HealthVault.Fhir.UnitTests.ToHealthVaultTests
                 list.Add(fhirParse.Parse<Observation>(sample).ToHealthVault());
             }
 
-            Assert.AreEqual(2, list.Count);
+            Assert.AreEqual(3, list.Count);
+
             Assert.AreEqual(BloodGlucose.TypeId, list[0].TypeId);
             Assert.IsTrue(list[0] is BloodGlucose);
+
             Assert.AreEqual(Weight.TypeId, list[1].TypeId);
-            Assert.IsTrue(list[1] is Weight);        
+            Assert.IsTrue(list[1] is Weight);
+
+            Assert.AreEqual(Height.TypeId, list[2].TypeId);
+            Assert.IsTrue(list[2] is Height);
         }
     }
 }
