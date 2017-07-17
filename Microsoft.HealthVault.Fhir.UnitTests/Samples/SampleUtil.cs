e33226174bdd42ebe34d04be377bc3aa5f02eeae
Brandon Pollett <brandon.pollett@gmail.com>
2017-07-17 14:03:25 -0700

    Adding missing access modifier

--- Microsoft.HealthVault.Fhir.UnitTests/Samples/SampleUtil.cs
+++ Microsoft.HealthVault.Fhir.UnitTests/Samples/SampleUtil.cs
@@ -11,7 +11,7 @@ using System.Reflection;
 
 namespace Microsoft.HealthVault.Fhir.UnitTests.Samples
 {
-    class SampleUtil
+    internal class SampleUtil
     {
         /// <summary>
         /// Gets file stored in the Samples directory and marked as embedded resource
