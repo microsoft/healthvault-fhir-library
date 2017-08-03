7327a89b398aa5e3991911a119606cac60e69b67
Brandon Pollett <brandon.pollett@gmail.com>
2017-08-03 10:07:27 -0700

    Adding missing copyright.

--- Microsoft.HealthVault.Fhir.UnitTests/ToFhirTests/BodyDimensionToFhirTests.cs
+++ Microsoft.HealthVault.Fhir.UnitTests/ToFhirTests/BodyDimensionToFhirTests.cs
@@ -1,8 +1,15 @@
-﻿using System;
+﻿// Copyright (c) Microsoft Corporation.  All rights reserved.
+// MIT License
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the ""Software""), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+using System;
 using Hl7.Fhir.Model;
 using Microsoft.HealthVault.Fhir.Transformers;
 using Microsoft.HealthVault.ItemTypes;
-using Microsoft.HealthVault.Thing;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Microsoft.HealthVault.Fhir.UnitTests.ToFhirTests
@@ -13,7 +20,11 @@ namespace Microsoft.HealthVault.Fhir.UnitTests.ToFhirTests
         [TestMethod]
         public void WhenHealthVaultBodyDimensionTransformedToFhir_ThenCodeAndValuesEqual()
         {
-            var bodyDimension = new BodyDimension(new ApproximateDateTime(new DateTime(2017,8,2,11,13,14)),new CodableValue("Left bicep size", new CodedValue("BicepCircumferenceLeft","body-dimension-measurement-names","wc", "1")),new Length(0.15));
+            var bodyDimension = new BodyDimension(
+                new ApproximateDateTime(new DateTime(2017,8,2,11,13,14)),
+                new CodableValue("Left bicep size", 
+                new CodedValue("BicepCircumferenceLeft","body-dimension-measurement-names","wc", "1")),new Length(0.15)
+                );
 
             var observation = bodyDimension.ToFhir();
 
