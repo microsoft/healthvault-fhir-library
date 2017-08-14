b9e25955cfc4a760c58dc7abcd15a8d6ff3521b4
Brandon Pollett <brandon.pollett@gmail.com>
2017-08-14 15:48:37 -0700

    Add basic to fhir transformer

---
+++ Microsoft.HealthVault.Fhir.UnitTests/ToFhirTests/BasicToFhirTests.cs
@@ -0,0 +1,59 @@
+﻿// Copyright (c) Microsoft Corporation.  All rights reserved.
+// MIT License
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the ""Software""), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+using System;
+using System.Linq;
+using Hl7.Fhir.Model;
+using Microsoft.HealthVault.Fhir.Transformers;
+using Microsoft.HealthVault.ItemTypes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Microsoft.HealthVault.Fhir.ToFhirTests.UnitTests
+{
+    [TestClass]
+    public class BasicToFhirTests
+    {
+        [TestMethod]
+        public void WhenHealthVaultBasicTransformedToFhir_ThenCodeAndValuesEqual()
+        {
+            var basic = new ItemTypes.Basic
+            {
+                Gender = Gender.Female,
+                BirthYear = 1975,
+                City = "Redmond",
+                StateOrProvince = "WA",
+                PostalCode = "98052",
+                Country = "USA",
+                FirstDayOfWeek = DayOfWeek.Sunday,
+                Languages =
+                {
+                    new Language(new CodableValue("English", "en", "iso639-1", "iso", "1"), true),
+                    new Language(new CodableValue("French", "fr", "iso639-1", "iso", "1"), false),
+                }
+            };
+
+            var patient = basic.ToFhir<Patient>();
+
+            Assert.IsNotNull(patient);
+            Assert.AreEqual(AdministrativeGender.Female, patient.Gender.Value);
+            Assert.AreEqual(1975, ((FhirDecimal)patient.Extension.First(x => x.Url == "https://healthvault.com/extensions/birth-year").Value).Value);
+            Assert.AreEqual("0", ((Coding)patient.Extension.First(x => x.Url == "https://healthvault.com/extensions/first-day-of-week").Value).Code);
+            Assert.AreEqual("Sunday", ((Coding)patient.Extension.First(x => x.Url == "https://healthvault.com/extensions/first-day-of-week").Value).Display);
+
+            Assert.AreEqual(1, patient.Address.Count);
+            Assert.AreEqual("Redmond", patient.Address[0].City);
+            Assert.AreEqual("WA", patient.Address[0].State);
+            Assert.AreEqual("98052", patient.Address[0].PostalCode);
+            Assert.AreEqual("USA", patient.Address[0].Country);
+
+            Assert.AreEqual(2, patient.Communication.Count);
+            Assert.AreEqual("English", patient.Communication[0].Language.Coding[0].Display);
+            Assert.AreEqual(true, patient.Communication[0].Preferred);
+        }
+    }
+}
---
+++ Microsoft.HealthVault.Fhir/Transformers/BasicToFhir.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Hl7.Fhir.Model;
+using Microsoft.HealthVault.Fhir.Codings;
+using Microsoft.HealthVault.Fhir.Constants;
+using Microsoft.HealthVault.ItemTypes;
+
+namespace Microsoft.HealthVault.Fhir.Transformers
+{
+    public static partial class ThingBaseToFhir
+    {
+        // Register the type on the generic ThingToFhir partial class
+        public static Patient ToFhir(this ItemTypes.Basic basic)
+        {
+            return BasicToFhir.ToFhirInternal(basic, ThingBaseToFhir.ToFhirInternal<Patient>(basic));
+        }
+    }
+
+    /// <summary>
+    /// An extension class that transforms HealthVault height data types into FHIR Observations
+    /// </summary>
+    internal static class BasicToFhir
+    {
+        internal static Patient ToFhirInternal(ItemTypes.Basic basic, Patient patient)
+        {
+            if (basic.Gender.HasValue)
+            {
+                switch (basic.Gender.Value)
+                {
+                    case Gender.Male:
+                        patient.Gender = AdministrativeGender.Male;
+                        break;
+                    case Gender.Female:
+                        patient.Gender = AdministrativeGender.Female;
+                        break;
+                    case Gender.Unknown:
+                        patient.Gender = AdministrativeGender.Unknown;
+                        break;
+                }
+            }
+
+            if (basic.BirthYear.HasValue)
+            {
+                patient.Extension.Add(new Extension
+                {
+                    Url = "https://healthvault.com/extensions/birth-year",
+                    Value = new FhirDecimal(basic.BirthYear)
+                });
+            }
+
+            if (basic.FirstDayOfWeek.HasValue)
+            {
+                patient.Extension.Add(new Extension
+                {
+                    Url = "https://healthvault.com/extensions/first-day-of-week",
+                    Value = new Coding
+                    {
+                        Code = ((int)basic.FirstDayOfWeek).ToString(),
+                        Display = basic.FirstDayOfWeek.Value.ToString()
+                    }
+                });
+            }
+
+            if (basic.Languages != null && basic.Languages.Count > 0)
+            {
+                foreach (var language in basic.Languages)
+                {
+                    patient.Communication.Add(new Patient.CommunicationComponent
+                    {
+                        Language = new CodeableConcept
+                        {
+                            Coding = HealthVaultCodesToFhir.ConvertCodableValueToFhir(language.SpokenLanguage, null)
+                        },
+                        Preferred = language.IsPrimary
+                    });
+                }
+            }
+
+            patient.Address.Add(new Hl7.Fhir.Model.Address
+            {
+                City = basic.City,
+                State = basic.StateOrProvince,
+                PostalCode = basic.PostalCode,
+                Country = basic.Country
+            });
+
+            return patient;
+        }
+    }
+}
