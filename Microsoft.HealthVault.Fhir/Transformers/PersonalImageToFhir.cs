6497282a534b2da751dd508a61568397135bef8e
Brandon Pollett <brandon.pollett@gmail.com>
2017-08-15 09:30:58 -0700

    Add personal image to fhir transformer

---
+++ Microsoft.HealthVault.Fhir/Transformers/PersonalImageToFhir.cs
@@ -0,0 +1,50 @@
+﻿// Copyright (c) Microsoft Corporation.  All rights reserved.
+// MIT License
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the ""Software""), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+using System.Collections.Generic;
+using Hl7.Fhir.Model;
+using Hl7.Fhir.Support;
+using Microsoft.HealthVault.Fhir.Constants;
+using Microsoft.HealthVault.ItemTypes;
+
+namespace Microsoft.HealthVault.Fhir.Transformers
+{
+    public static partial class ThingBaseToFhir
+    {
+        // Register the type on the generic ThingToFhir partial class
+        public static Patient ToFhir(this PersonalImage personalImage)
+        {
+            return PersonalImageToFhir.ToFhirInternal(personalImage, ThingBaseToFhir.ToFhirInternal<Patient>(personalImage));
+        }
+    }
+
+    /// <summary>
+    /// An extension class that transforms HealthVault personal image data types into FHIR Observations
+    /// </summary>
+    internal static class PersonalImageToFhir
+    {
+        internal static Patient ToFhirInternal(PersonalImage personalImage, Patient patient)
+        {
+            using (var stream = personalImage.ReadImage())
+            {
+                var bytes = new byte[stream.Length];
+                stream.Read(bytes, 0, (int)stream.Length);
+
+                if (!bytes.IsNullOrEmpty())
+                {
+                    patient.Photo.Add(new Attachment
+                    {
+                        Data = bytes
+                    });
+                }
+            }   
+
+            return patient;
+        }
+    }
+}
