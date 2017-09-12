743ee2da793b6c3b6500c177420ebd5f3fc387a5
Mahesh V S <mahesh@getrealhealth.com>
2017-09-12 18:24:32 +0530

    Improving Test
    Adding necessary code blocks to cover secondary provider
    Fixing minor issues in various assertions

--- Microsoft.HealthVault.Fhir.UnitTests/ToHealthVaultTests/FhirProcedureToHealthVaultProcedure.cs
+++ Microsoft.HealthVault.Fhir.UnitTests/ToHealthVaultTests/FhirProcedureToHealthVaultProcedure.cs
@@ -1,10 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// Copyright (c) Get Real Health.  All rights reserved.
+// MIT License
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the ""Software""), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 using Hl7.Fhir.Model;
 using Hl7.Fhir.Serialization;
+using Microsoft.HealthVault.Fhir.Transformers;
 using Microsoft.HealthVault.Fhir.UnitTests.Samples;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -13,6 +17,7 @@ namespace Microsoft.HealthVault.Fhir.UnitTests.ToHealthVaultTests
     [TestClass]
     public class FhirProcedureToHealthVaultProcedure
     {
+        [TestCategory("Procedure")]
         [TestMethod]
         public void WhenFhirProcedureToHealthVaultProcedure_ThenValuesEqual()
         {
@@ -32,12 +37,16 @@ namespace Microsoft.HealthVault.Fhir.UnitTests.ToHealthVaultTests
             Assert.AreEqual(00, hvProcedure.When.ApproximateTime.Second);
 
             //name
-            Assert.AreEqual("Chemotherapy", hvProcedure.Name.Text);
-            //TODO: Assert code and system of hvProcedure.Name
+            Assert.AreEqual("Chemotherapy", hvProcedure.Name.Text);            
 
             //primary-provider
-            Assert.AreEqual("Dokter Bronsig", hvProcedure.PrimaryProvider.Name.Full);
-            Assert.AreEqual("Medical oncologist", hvProcedure.PrimaryProvider.ProfessionalTraining);
+            Assert.AreEqual("Adam", hvProcedure.PrimaryProvider.Name.First);
+            Assert.AreEqual("Careful", hvProcedure.PrimaryProvider.Name.Last);
+            Assert.AreEqual("Certified Medical Assistant", hvProcedure.PrimaryProvider.ProfessionalTraining);
+
+            //secondary-provider
+            Assert.AreEqual("Dokter Bronsig", hvProcedure.SecondaryProvider.Name.Full);            
+
         }
     }
 }
