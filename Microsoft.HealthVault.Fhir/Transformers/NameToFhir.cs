cfc58df9069ba68805bb1ad9c2cc0f3ee81d93be
Mahesh V S <mahesh@getrealhealth.com>
2017-09-20 17:46:21 +0530

    Adding transformation for few basic ItemTypes
    Types: Address, Email, Name and Phone

---
+++ Microsoft.HealthVault.Fhir/Transformers/NameToFhir.cs
@@ -0,0 +1,58 @@
+﻿// Copyright (c) Get Real Health.  All rights reserved.
+// MIT License
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the ""Software""), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+using System.Collections.Generic;
+using Hl7.Fhir.Model;
+using Microsoft.HealthVault.ItemTypes;
+
+namespace Microsoft.HealthVault.Fhir.Transformers
+{
+    public static partial class ItemBaseToFhir
+    {
+        public static HumanName ToFhir(this Name name)
+        {
+            return NameToFhir.ToFhirInternal(name);
+        }
+    }
+    internal static class NameToFhir
+    {
+        internal static HumanName ToFhirInternal(Name hvName)
+        {
+            if (hvName == null)
+                return null;
+
+            var fhirName = new HumanName
+            {
+                Text = hvName.Full,
+                Family = hvName.Last
+            };
+
+            if (hvName.Title != null)
+            {
+                fhirName.Prefix = new List<string> { hvName.Title.Text };
+            }
+
+            AddGivenName(fhirName, hvName.First);
+            AddGivenName(fhirName, hvName.Middle);
+
+            if (hvName.Suffix != null)
+            {
+                fhirName.Suffix = new List<string> { hvName.Suffix.Text };
+            }
+
+            return fhirName;
+        }
+
+        private static void AddGivenName(HumanName fhirName, string name)
+        {
+            if (!string.IsNullOrEmpty(name))
+            {
+                fhirName.GivenElement.Add(new FhirString(name));
+            }
+        }
+    }
+}
---
+++ Microsoft.HealthVault.Fhir/Transformers/PhoneToFhir.cs
@@ -0,0 +1,40 @@
+﻿// Copyright (c) Get Real Health.  All rights reserved.
+// MIT License
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the ""Software""), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+using Hl7.Fhir.Model;
+using Microsoft.HealthVault.Fhir.Constants;
+using Microsoft.HealthVault.ItemTypes;
+
+namespace Microsoft.HealthVault.Fhir.Transformers
+{
+    public static partial class ItemBaseToFhir
+    {
+        public static ContactPoint ToFhir(this Phone phone)
+        {
+            return PhoneToFhir.ToFhirInternal(phone);
+        }
+    }
+    internal class PhoneToFhir
+    {
+        internal static ContactPoint ToFhirInternal(Phone phone)
+        {
+            var contactPoint = new ContactPoint
+            {
+                System = ContactPoint.ContactPointSystem.Phone,
+                Value = phone.Number,
+                Rank = phone.IsPrimary.HasValue && phone.IsPrimary.Value ? 1 : (int?)null,
+            };
+
+            if (!string.IsNullOrEmpty(phone.Description))
+            {
+                contactPoint.AddExtension(HealthVaultExtensions.Description, new FhirString(phone.Description));
+            }
+
+            return contactPoint;
+        }
+    }
+}
